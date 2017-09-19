878ed471c1fc9f12a348ee1c8acd75800cafe6c2
Alexander Westphal <westphal.alex94@gmail.com>
2017-09-20 01:00:04 +0300

    Disable dashboard suggestions [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3776,6 +3776,12 @@ public final class Settings {
         public static final String ENABLE_CONDITIONS = "enable_conditions";
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String DISABLE_SUGGESTIONS = "disable_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
