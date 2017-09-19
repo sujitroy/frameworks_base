5b9f0cd8fb069dcc2e60b7de4f7c0c8aeb1c83d6
xyyx <xyyx@mail.ru>
2017-09-19 23:43:24 +0300

    Ability to disable dashboards conditions (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3770,6 +3770,12 @@ public final class Settings {
         public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
