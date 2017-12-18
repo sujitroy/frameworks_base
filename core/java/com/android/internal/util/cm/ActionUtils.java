cf9310f4ff901fdba44b6e3245c34a2716b05828
bigrushdog <randall.rushing@gmail.com>
2017-12-18 12:13:21 +0200

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to ActionUtils

--- core/java/com/android/internal/util/cm/ActionUtils.java
+++ core/java/com/android/internal/util/cm/ActionUtils.java
@@ -13,7 +13,9 @@ import android.os.RemoteException;
 import android.os.UserHandle;
 import android.util.Log;
 
+import android.view.IWindowManager;
 import android.view.HapticFeedbackConstants;
+import android.view.WindowManagerGlobal;
 import android.widget.Toast;
 import com.android.internal.R;
 
@@ -23,6 +25,18 @@ public class ActionUtils {
     private static final boolean DEBUG = false;
     private static final String TAG = ActionUtils.class.getSimpleName();
     private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Kills the top most / most recent user application, but leaves out the launcher.
